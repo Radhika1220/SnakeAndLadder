4f010669b8f7e59c06512d028ad0eefa96c7066c
Radhika <radhika.shankar1220@gmail.com>
2021-06-29 14:44:32 +0530

    [Radhika] Exact winning position 100

--- SnakeAndLadder1/Program.cs
+++ SnakeAndLadder1/Program.cs
@@ -3,18 +3,22 @@
 namespace SnakeAndLadder1
 {
     class Program
-    {/// <summary>
-     /// Created Rolldice function and checkingoptions 
-    /// UC4-Repeat till position reached 100
-    /// </summary>
-    
+    {
+        /// <summary>
+        ///UC1-Initializing a player position
+        ///UC2-Creating a random function for rolling the dice
+        ///UC3-Checking Options
+        /// UC4-Repeat till position reached 100
+        /// UC5-Stop at 100 position
+        /// </summary>
+
         //Declaring Constant variables
         public const int SIZE = 100;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake And Ladder Problem!");
 
-            
+
             //Calling a checkingoptions function 
             Program.CheckingOptions();
         }
@@ -22,17 +26,17 @@ namespace SnakeAndLadder1
         {
             //Creating a random function
             Random random = new Random();
-           int rollDice = random.Next(1, 7);
-            
+            int rollDice = random.Next(1, 7);
+
             return rollDice;
         }
-       
+
         public static void CheckingOptions()
         {
             Random r = new Random();
-            
+
             int playerPosition = 0;
-            
+
             while (playerPosition <= SIZE)
             {
                 int check = r.Next(1, 4);
@@ -49,21 +53,30 @@ namespace SnakeAndLadder1
                         Console.WriteLine("No Play");
                         break;
                     case 2:
-                       
+
                         Console.WriteLine("Ladder Played");
-                        playerPosition += dice;
+                        if (playerPosition + dice <= 100)
+                        {
+                            playerPosition += dice;
+                        }
+
                         Console.WriteLine("Player Current Position " + playerPosition);
                         break;
                     case 3:
-                        
+
                         Console.WriteLine("Snake Played");
                         playerPosition -= dice;
-                        if(playerPosition<0)
+                        if (playerPosition < 0)
                         {
                             playerPosition = 0;
                         }
                         Console.WriteLine("Player Current Position " + playerPosition);
                         break;
+                }
+
+                if (playerPosition == 100)
+                {
+                    break;
                 }
             }
         }
