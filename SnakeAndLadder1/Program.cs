3e7d6c6dffda6aee43b5c30e303a66e6801b0db8
Radhika <radhika.shankar1220@gmail.com>
2021-06-29 13:46:05 +0530

    [Radhika] Repeat till position reach 100

--- SnakeAndLadder1/Program.cs
+++ SnakeAndLadder1/Program.cs
@@ -3,46 +3,68 @@
 namespace SnakeAndLadder1
 {
     class Program
-    {
-        
-
+    {/// <summary>
+     /// Created Rolldice function and checkingoptions 
+    /// UC4-Repeat till position reached 100
+    /// </summary>
+    
+        //Declaring Constant variables
+        public const int SIZE = 100;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake And Ladder Problem!");
 
-            //Program.RollDice();
+            
+            //Calling a checkingoptions function 
             Program.CheckingOptions();
         }
         public static int RollDice()
         {
+            //Creating a random function
             Random random = new Random();
            int rollDice = random.Next(1, 7);
-            //Console.WriteLine("Dice number : "+rollDice);
+            
             return rollDice;
         }
        
         public static void CheckingOptions()
         {
             Random r = new Random();
-            int check = r.Next(1, 4);
+            
             int playerPosition = 0;
-            int dice = Program.RollDice();
-            Console.WriteLine("Dice number is " + dice);
-            switch(check)
+            
+            while (playerPosition <= SIZE)
             {
-                case 1:
-                    Console.WriteLine("No Play");
-                    break;
-                case 2:
-                     Console.WriteLine("Ladder Played");
-                     playerPosition +=dice;
-                     Console.WriteLine("Player Current Position " + playerPosition);
-                     break;
-                case 3:
-                    Console.WriteLine("Snake Played");
-                    playerPosition -= dice;
-                    Console.WriteLine("Player Current Position " + playerPosition);
-                    break;
+                int check = r.Next(1, 4);
+
+
+                //Calling a rolldice function
+
+                int dice = Program.RollDice();
+                Console.WriteLine("Dice number is " + dice);
+
+                switch (check)
+                {
+                    case 1:
+                        Console.WriteLine("No Play");
+                        break;
+                    case 2:
+                       
+                        Console.WriteLine("Ladder Played");
+                        playerPosition += dice;
+                        Console.WriteLine("Player Current Position " + playerPosition);
+                        break;
+                    case 3:
+                        
+                        Console.WriteLine("Snake Played");
+                        playerPosition -= dice;
+                        if(playerPosition<0)
+                        {
+                            playerPosition = 0;
+                        }
+                        Console.WriteLine("Player Current Position " + playerPosition);
+                        break;
+                }
             }
         }
     }
