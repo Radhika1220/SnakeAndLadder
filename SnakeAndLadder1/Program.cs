d27fe0f4c24749bd551e2330acd7a20450f4e0f8
Radhika <radhika.shankar1220@gmail.com>
2021-06-29 10:28:26 +0530

    [Radhika] Commiting a welcome message

---
+++ SnakeAndLadder1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeAndLadder1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake And Ladder Problem!");
+        }
+    }
+}
